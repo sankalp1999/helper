e046596b0d3472c67bc8fe83bbcab696bf6b99eb
sankalp1999 <45618047+sankalp1999@users.noreply.github.com>
2025-06-22 03:59:04 +0530

    feat(ux improvement): tooltip will show when main sidebar is collapsed

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -358,7 +358,7 @@ const SidebarMenuButton = React.forwardRef<
   } & VariantProps<typeof sidebarMenuButtonVariants>
 >(({ asChild = false, isActive = false, variant = "default", size = "default", tooltip, className, ...props }, ref) => {
   const Comp = asChild ? Slot : "button";
-  const { isMobile } = useSidebar();
+  const { isMobile, open } = useSidebar();
 
   const button = (
     <Comp
@@ -384,7 +384,7 @@ const SidebarMenuButton = React.forwardRef<
   return (
     <Tooltip>
       <TooltipTrigger asChild>{button}</TooltipTrigger>
-      <TooltipContent side="right" align="center" hidden={isMobile} {...tooltip} />
+      <TooltipContent side="right" align="center" hidden={isMobile || open} {...tooltip} />
     </Tooltip>
   );
 });
